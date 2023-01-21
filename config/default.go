67f7210137c16b8074b55ca285be201e971b58a6
triskil <trisskil@gmail.com>
2023-01-21 10:52:39 -0500

    Initial commit.

---
+++ config/default.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBUri               string `mapstructure:"MONGODB_LOCAL_URI"`
+	Port                string `mapstructure:"PORT"`
+	JWTSecretKey        string `mapstructure:"JWT_SECRET_KEY"`
+	JWTRefreshSecretKey string `mapstructure:"JWT_REFRESH_SECRET_KEY"`
+}
+
+var (
+	AppConfig *Config
+)
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("app")
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
